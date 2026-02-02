9ff64a21cb18dcbf196e464b67bbc3f3606885cc
Muhammad Aqib javeed <maqibjaveed4@gmail.com>
2026-02-02 16:21:16 +0500

    Adding the Data

--- user_db.py
+++ user_db.py
@@ -15,20 +15,22 @@ cursor.execute("USE user_authentication")
 # Table Created
 cursor.execute("""CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(70) UNIQUE 
+    username VARCHAR(70) UNIQUE,
     password VARCHAR(100) 
-
+)
 """)
 
 print("Successfully Table Created")
-'''
-# We Use Loop For Register 
-while True:
-    print(" 1. Register\n 2. Login\n 3. Exit")
+
+# We Use Loop For Register And Login
+
+print(" 1. Register\n 2. Login\n 3. Exit")
+
 
     # That we Use 3 Entry to put Develope the Data
-    choice = input(" Enter your choice (1/2/3): ")
 
+while True:
+    choice = input(" Enter your choice (1/2/3): ")
     if choice == '1':
         username = input("Enter username: ")
         password = input("Enter password: ")
@@ -36,21 +38,34 @@ while True:
         # Check if username already exists
         cursor.execute(" SELECT * FROM users WHERE username = %s", (username,))
         if cursor.fetchone():
-            print(" Username already exists. Please choose a different username.")
-        continue
-
-        # Insert new user
-        cursor.execute(" INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
+            print(" Username already exists Please choose a different username.")
+                
+        # Use Else For Insert Data iF Data is Not Exist
+        else:
+            cursor.execute(" INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
+            print(" Successfully Registered!")      
         conn.commit()
-    print(" Registration successful!")
-    
+        
     elif choice == '2':
-        username = input(" Enter username: ")
-        password = input(" Enter password: ")
+        username = input("Enter username: ")
+        password = input("Enter password: ")
 
         # Verify user credentials
         cursor.execute(" SELECT * FROM users WHERE username = %s AND password = %s", (username, password))
         if cursor.fetchone():
             print(" Login successful!")
         else:
-            print(" Invalid username or password.")'''
+            print("Invalid username or password.....")
+
+    elif choice == '3':
+        print(" Exiting....")
+        
+        break
+                    
+    else:
+        print("Invalid choice. Please try again...")        
+        
+# Connection Close
+cursor.close()
+conn.close()
+    
